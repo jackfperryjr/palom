7af72b8478d2e4062f81c5e7e9c6e5aeca9cf98f
Jack <jackfperryjr@gmail.com>
2020-12-18 11:33:42 -0500

    Trying something else.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,6 +7,8 @@ const fs = require("fs");
     console.log("Building...");
     await execa("npm", ["run", "build"]);
     const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["config", "user.name", "jackfperryjr"])
+    await execa("git", ["config", "user.email", "jackfperryjr@gmail.com"])
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
